61870dbc75f16cfcad6137e39f4044b270cb972c
Christian Cattaneo <christian.cattaneo@gauntletai.com>
2025-01-11 18:15:24 -0500

    Configure Neon database

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3fc065e691cfc6c7"; }
+/******/ 		__webpack_require__.h = function() { return "c20e52e2fa385188"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
